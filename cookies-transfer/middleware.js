1a61b9864a354a6ddcafc1009d8533b52d87e232
Luxam <luxamrownn@gmail.com>
2025-11-18 09:39:04 +0700

    update

--- cookies-transfer/middleware.js
+++ cookies-transfer/middleware.js
@@ -4,19 +4,19 @@ import { NextResponse } from 'next/server';
 export function middleware(request) {
   const response = NextResponse.rewrite("https://sub-cookie-transfer.vercel.app/");
 
-  response.cookies.set('rewrite_cookie', 'METS11223344', {
-    maxAge: 60 * 60 * 24, // 1 day
-    httpOnly: true,
-    path: '/',
-    domain: ".sub-cookie-transfer.vercel.app"
-  });
-
-//     response.cookies.set('rewrite_cookie', 'MATS11223344', {
+//   response.cookies.set('rewrite_cookie', 'METS11223344', {
 //     maxAge: 60 * 60 * 24, // 1 day
 //     httpOnly: true,
 //     path: '/',
-//     domain: "sub-cookie-transfer.vercel.app"
+//     domain: ".sub-cookie-transfer.vercel.app"
 //   });
+
+    response.cookies.set('rewrite_cookie', 'MATS11223344', {
+    maxAge: 60 * 60 * 24, // 1 day
+    httpOnly: true,
+    path: '/',
+    domain: "sub-cookie-transfer.vercel.app"
+  });
 
 
 //     response.cookies.set('rewrite_cookie_int', 'LATS123123213321', {
