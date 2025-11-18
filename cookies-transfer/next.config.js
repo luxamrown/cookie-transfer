d88ef6278a66159eb80359b8a56b79bc4c3ddef3
Luxam <luxamrownn@gmail.com>
2025-11-18 08:52:35 +0700

    update

--- cookies-transfer/next.config.js
+++ cookies-transfer/next.config.js
@@ -1,24 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-function getMainDomain() {
-
-  // Priority 2: Vercel auto domain
-  if (process.env.VERCEL_URL) {
-    return normalizeDomain(process.env.VERCEL_URL);
-  }
-
-  // Priority 3: Local fallback
-  return "localhost:3001";
-}
 
 const nextConfig = {
   async rewrites() {
-    const main = process.env.VERCEL_URL;
 
     return [
       {
         source: "/sub/:path*",
-        destination: `https://${main}/:path*`,
+        destination: `https://sub-cookie-transfer.vercel.app/:path*`,
       },
     ];
   },
